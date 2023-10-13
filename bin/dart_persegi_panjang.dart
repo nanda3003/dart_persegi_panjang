18d9691d3f03d48b16d18c5474193e44f527e922
nanda3003 <nan.id@mhs.mdp.ac.id>
2023-10-13 12:52:33 +0700

    Initial commit

---
+++ bin/dart_persegi_panjang.dart
@@ -0,0 +1,5 @@
+import 'package:dart_persegi_panjang/dart_persegi_panjang.dart' as dart_persegi_panjang;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_persegi_panjang.calculate()}!');
+}
---
+++ test/dart_persegi_panjang_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_persegi_panjang/dart_persegi_panjang.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
